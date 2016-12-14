a621fe401a30418e48dc7bd294e2325e1e7a7acf
18775134221 <18775134221@163.com>
2016-12-14 17:54:37 +0800

    新增拨打电话的工具类

--- SwiftTest/SwiftTest/Classes/Types/VC/TypesVC.swift
+++ SwiftTest/SwiftTest/Classes/Types/VC/TypesVC.swift
@@ -22,14 +22,10 @@ class TypesVC: BaseVC {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        JQPhoneManagerVC.call("10086", self, failBlock: {
+            print("拨打失败")
+        })
     }
-    */
 
 }
---
+++ SwiftTest/SwiftTest/Classes/tools/JQPhoneManagerVC.swift
@@ -0,0 +1,78 @@
+//
+//  JQPhoneManagerVC.swift
+//  SwiftTest
+//
+//  Created by MAC on 2016/12/14.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - 拨打电话
+class JQPhoneManagerVC: UIViewController {
+
+    
+    var webView: UIWebView?
+    
+    init () {
+        webView = UIWebView()
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.removeFromParentViewController()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    /**
+     *  打电话
+     *
+     *  @param no                   电话号码
+     *  @param inViewController     需要打电话的控制器
+     */
+    class func call(_ no: String, _ inViewController: UIViewController?,failBlock: ()->()) {
+    
+        // 拨打电话
+        let noString: String = "tel://" + no
+        let url: NSURL = NSURL(string: noString)!;
+        let canOpen: Bool = UIApplication.shared.openURL(url as URL)
+        if canOpen == false {
+            failBlock()
+            return
+        }
+        
+        let pMVC: JQPhoneManagerVC = JQPhoneManagerVC()
+        pMVC.view.alpha = 0
+        pMVC.view.frame = CGRect.zero
+        inViewController?.addChildViewController(pMVC)
+        
+        inViewController?.view.addSubview(pMVC.view)
+        
+        let request: NSURLRequest = NSURLRequest(url: url as URL)
+        pMVC.webView?.loadRequest(request as URLRequest)
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    deinit {
+        webView = nil
+        print("PhoneManager已经被释放")
+    }
+
+}
